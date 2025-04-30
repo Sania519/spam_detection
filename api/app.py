6991420ee85fbcc18495160793fbce2be666b6c9
Sania519 <brsedlabadkar@gmail.com>
2025-04-30 00:51:10 -0700

    Initial Commit

--- api/app.py
+++ api/app.py
@@ -147,4 +147,4 @@ def get_model(model_id):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
--- client/client_ui.py
+++ client/client_ui.py
@@ -225,4 +225,4 @@ def clear_model():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=80)
